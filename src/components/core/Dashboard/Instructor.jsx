fcef918a5dc6107c17fe6f72f42debfecf75344c
abhikant12 <abhiparnav12@gmail.com>
2023-09-26 12:17:40 +0530

    added project files

--- src/components/core/Dashboard/Instructor.jsx
+++ src/components/core/Dashboard/Instructor.jsx
@@ -99,11 +99,6 @@ export default function Instructor(){
                   <img src={course?.thumbnail} alt={course?.courseName} className="h-[201px] w-full rounded-md object-cover"/>
                   <div className="mt-3 w-full">
                     <p className="text-sm font-medium text-richblack-50"> {course?.courseName} </p>
-                    <div className="mt-1 flex items-center space-x-2">
-                      <p className="text-xs font-medium text-richblack-300">{course?.studentsEnroled?.length} students </p>
-                      <p className="text-xs font-medium text-richblack-300"> |  </p>
-                      <p className="text-xs font-medium text-richblack-300"> Rs. {course?.price}</p>
-                    </div>
                   </div>
 
                 </div>
