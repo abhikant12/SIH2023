94acd439cac8982db5016856678d6431667e35d4
abhikant12 <abhiparnav12@gmail.com>
2023-09-29 11:45:30 +0530

    added project

--- src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
@@ -140,44 +140,61 @@ export default function CourseInformationForm(){
        
       {/* Course Title */}
       <div className="flex flex-col space-y-2">
-        <label className="text-sm text-richblack-5" htmlFor="courseTitle"> Resources Title <sup className="text-pink-200">*</sup> </label>
+        <label className="text-sm text-richblack-5" htmlFor="courseTitle"> Respository Title <sup className="text-pink-200">*</sup> </label>
         <input  id="courseTitle" placeholder="Enter Course Title" {...register("courseTitle", { required: true })} className="form-style w-full" />
         {errors.courseTitle && (<span className="ml-2 text-xs tracking-wide text-pink-200"> Course title is required </span> )}
       </div>
      
       {/* Course Short Description */}
       <div className="flex flex-col space-y-2">
-        <label className="text-sm text-richblack-5" htmlFor="courseShortDesc"> Resources Short Description <sup className="text-pink-200">*</sup>  </label>
+        <label className="text-sm text-richblack-5" htmlFor="courseShortDesc"> Respository Short Description <sup className="text-pink-200">*</sup>  </label>
         <textarea id="courseShortDesc" placeholder="Enter Description"  {...register("courseShortDesc", { required: true })} className="form-style resize-x-none min-h-[130px] w-full" />
         {errors.courseShortDesc && (<span className="ml-2 text-xs tracking-wide text-pink-200"> Course Description is required</span> )}        
       </div>
-     
+    
+      {/* Course Price */}
+      <div className="flex flex-col space-y-2">
+        <label className="text-sm text-richblack-5" htmlFor="coursePrice">  Respository Price <sup className="text-pink-200">*</sup> </label>
+        <div className="relative">
+          <input id="coursePrice"  placeholder="Enter Course Price" className="form-style w-full !pl-12"
+              {...register("coursePrice", {
+                required: true,
+                valueAsNumber: true,
+                pattern: {
+                  value: /^(0|[1-9]\d*)(\.\d+)?$/,
+                },
+              })}
+          />
+          <HiOutlineCurrencyRupee className="absolute left-3 top-1/2 inline-block -translate-y-1/2 text-2xl text-richblack-400" />
+        </div>
+        {errors.coursePrice && (<span className="ml-2 text-xs tracking-wide text-pink-200"> Respository Price is required </span> )}
+      </div>
 
       {/* Course Category */}
       <div className="flex flex-col space-y-2">
-        <label className="text-sm text-richblack-5" htmlFor="courseCategory"> Resources Category <sup className="text-pink-200">*</sup> </label>
+        <label className="text-sm text-richblack-5" htmlFor="courseCategory"> Respository Category <sup className="text-pink-200">*</sup> </label>
   
         <select  {...register("courseCategory", { required: true })} defaultValue="" id="courseCategory" className="form-style w-full" >
-          <option value="" disabled> Choose a Resources  </option>
+          <option value="" disabled> Choose a Category  </option>
           {!loading &&
             courseCategories?.map((category, indx) => ( 
                         <option key={indx} value={category?._id}> {category?.name} </option> 
           ))}
         </select>
       
-        {errors.courseCategory && (<span className="ml-2 text-xs tracking-wide text-pink-200">  Resources Category is required </span>)}
+        {errors.courseCategory && (<span className="ml-2 text-xs tracking-wide text-pink-200">  Course Category is required </span>)}
       </div>
     
       {/* Course Tags */}
       <ChipInput  label="Tags"  name="courseTags" placeholder="Enter Tags and press Enter" register={register}  errors={errors} setValue={setValue} getValues={getValues} />
-              
+       
       {/* Course Thumbnail Image */}
-      <Upload name="courseImage" label="Resources Thumbnail" register={register} setValue={setValue} errors={errors}  editData={editCourse ? course?.thumbnail : null} />
+      <Upload name="courseImage" label="Respository Thumbnail" register={register} setValue={setValue} errors={errors}  editData={editCourse ? course?.thumbnail : null} />
      
       {/* Benefits of the course */}
       <div className="flex flex-col space-y-2">
         <label className="text-sm text-richblack-5" htmlFor="courseBenefits">
-          Benefits of this Resources <sup className="text-pink-200">*</sup>
+          Benefits of the Respository <sup className="text-pink-200">*</sup>
         </label>
        
         <textarea  id="courseBenefits"  placeholder="Enter benefits of the course"  {...register("courseBenefits", { required: true })} className="form-style resize-x-none min-h-[130px] w-full"  />
